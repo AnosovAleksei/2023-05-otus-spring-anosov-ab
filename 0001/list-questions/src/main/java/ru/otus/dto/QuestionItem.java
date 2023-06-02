a954d39f5fd8c5d46cc678fa4925f00eca1c4f1b
anosov_ab <anosov_ab@nlmk.com>
2023-06-02 23:21:48 +0300

    fixing problems

--- 0001/list-questions/src/main/java/ru/otus/dto/QuestionItem.java
+++ 0001/list-questions/src/main/java/ru/otus/dto/QuestionItem.java
@@ -9,24 +9,4 @@ public class QuestionItem {
     private String question;
 
     private List<Answer> answerOptions;
-
-    @Override
-    public String toString(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("question : ");
-        sb.append(question);
-        sb.append("\n");
-
-
-
-        sb.append("answer options : [");
-        sb.append(String.join(", ",  answerOptions.stream().map(s->s.getBody()).toList()));
-        sb.append("]");
-
-//        sb.append("correct answer : ");
-//        sb.append(String.join(", ",  answerOptions.stream().filter(s->s.isCorrect()).map(s->s.getBody()).toList()));
-
-
-        return sb.toString();
-    }
 }
